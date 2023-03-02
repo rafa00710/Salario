edb9d483bc3bc8852afe70813413a37e6da2301c
Rafael Pereira de Lima <rafael_pereira_007@hotmail.com>
2023-03-02 16:09:13 -0300

    Média salario

---
+++ Salario.java
@@ -0,0 +1,33 @@
+package Shadow;
+
+import javax.swing.*;
+
+public class Salario {
+    public static void main(String[] args) {
+        
+        int quantidadeFuncionarios;
+        float salarioFuncionario;
+        float totalSalarios = 0;
+        float mediaSalarial ;
+
+        //variável contadora para laço
+        
+        int contadora = 0;
+
+        quantidadeFuncionarios = Integer.parseInt(JOptionPane.showInputDialog(null,"Digite a quantidade de funcionários","Número de funcionários",JOptionPane.QUESTION_MESSAGE));
+
+
+        while (contadora < quantidadeFuncionarios){
+            contadora++;
+            salarioFuncionario = Float.parseFloat(JOptionPane.showInputDialog(null,"Digite o salário do funcionário","Salário",JOptionPane.QUESTION_MESSAGE));
+            totalSalarios = totalSalarios + salarioFuncionario;
+        }
+
+        mediaSalarial = totalSalarios / quantidadeFuncionarios;
+        System.out.println("Média Salarial = " + mediaSalarial);
+
+
+    }
+
+}
+
