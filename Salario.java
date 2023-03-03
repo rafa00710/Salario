57d31e520176ed206aa168d6009b8f6c7ce8913b
Rafael Pereira de Lima <rafael_pereira_007@hotmail.com>
2023-03-03 13:45:00 -0300

    Imcrementação de cargos.

--- Salario.java
+++ Salario.java
@@ -4,7 +4,8 @@ import javax.swing.*;
 
 public class Salario {
     public static void main(String[] args) {
-        
+
+        String cargo;
         int quantidadeFuncionarios;
         float salarioFuncionario;
         float totalSalarios = 0;
@@ -14,17 +15,21 @@ public class Salario {
         
         int contadora = 0;
 
-        quantidadeFuncionarios = Integer.parseInt(JOptionPane.showInputDialog(null,"Digite a quantidade de funcionários","Número de funcionários",JOptionPane.QUESTION_MESSAGE));
+        cargo = (JOptionPane.showInputDialog(null,"Digite o cargo do funcionário:","cargo",JOptionPane.QUESTION_MESSAGE));
+
+        quantidadeFuncionarios = Integer.parseInt(JOptionPane.showInputDialog(null,"Digite a quantidade de funcionários:","Número de funcionários",JOptionPane.QUESTION_MESSAGE));
 
 
         while (contadora < quantidadeFuncionarios){
             contadora++;
-            salarioFuncionario = Float.parseFloat(JOptionPane.showInputDialog(null,"Digite o salário do funcionário","Salário",JOptionPane.QUESTION_MESSAGE));
+            salarioFuncionario = Float.parseFloat(JOptionPane.showInputDialog(null,"Digite o salário do funcionário:","Salário",JOptionPane.QUESTION_MESSAGE));
             totalSalarios = totalSalarios + salarioFuncionario;
         }
 
         mediaSalarial = totalSalarios / quantidadeFuncionarios;
+        System.out.println("Cargo: " + cargo);
         System.out.println("Média Salarial = " + mediaSalarial);
+
 
 
     }
